6c1d8122571494e21d54afc9a58b3bab81dcf7a7
smuhatia <shawnmuhatia62@gmail.com>
2023-02-23 03:16:43 -0800

    fixing task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop and to fix the infite loop in the while loop we add 'i++;'
+* main - causes an infinite loop and to fix the infite loop in the while loop we increment i by adding "i++;"
 * Return: 0
 */
 
