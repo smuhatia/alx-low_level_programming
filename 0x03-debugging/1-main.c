1a6f376be2fcb1ca2842f1de5fd1ff55ce1db7bc
smuhatia <shawnmuhatia62@gmail.com>
2023-02-23 02:59:46 -0800

    debugging task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* To fix the infite loop in the while loop we add 'i++'
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}
