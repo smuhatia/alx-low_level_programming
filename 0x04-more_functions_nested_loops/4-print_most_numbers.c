57b84c63e364577121a827e7cab89c767af62e96
smuhatia <shawnmuhatia62@gmail.com>
2023-02-23 14:00:44 -0800

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/**main is the main function
- * 
- * description function that prints the numbers, from 0 to 9, followed by a new line.
- * return void
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
  */
 void print_most_numbers(void)
 {
