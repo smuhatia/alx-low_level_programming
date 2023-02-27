ffed5127101882e4495997a277f719957fc7bf11
smuhatia <shawnmuhatia62@gmail.com>
2023-02-27 10:00:09 -0800

    betty fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
 /**
- * main - function that takes a pointer to an int as parameter and updates the value it points to to 98
- *
- * Return nothing
- */
+* reset_to_98 - Takes a pointer to a parameter and updates its value
+* @n: input integer.
+*
+* Return: no return
+*/
+
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
