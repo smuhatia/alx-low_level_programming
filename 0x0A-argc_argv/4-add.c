a55d1f8b21d2e1f54f55b81296b9941a81c714e9
smuhatia <smuhatia>
2023-03-12 16:58:38 +0000

    program that adds

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,24 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int _is_digit(char *s);
 /**
-  * main - program that adds two numbers
+  * main - a program that adds positive numbers
   * @argc: number of arguments passed
   * @argv: vector of pointers to strings passed
   *
-  * Return: 0 (success) or -1 (error)
+  * Return: 0 (success)
   */
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, sum, digit;
 
-	if (argc == 3)
+	i = 1;
+	while (i < argc)
 	{
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-		printf("%d\n", i + j);
-
-		return (0);
+		if (_is_digit(argv[i]))
+		{
+			digit = atoi(argv[i]);
+			sum += digit;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
 	}
-	printf("Error\n");
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+/**
+  * _is_digit - compares values of string if its number
+  * @s: array of string
+  *
+  * Return: 0 (success)
+  */
+int _is_digit(char *s)
+{
+	int i, len;
+
+	i = 0;
+	len = strlen(s);
+	while (i < len)
+	{
+		if (!isdigit(s[i]))
+		{
+			return (0);
+		}
+		i++;
+	}
 	return (1);
 }
