26124ebfde9a91f5440a79d306d2f7942859b067
smuhatia <smuhatia>
2023-03-22 19:01:00 +0000

    whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: string to be printed
+ * @f: pointer to a function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	/* Pass the parameter "name" to function pointed to by "f"*/
+	/* Let the function pointed to by "f" do the printing */
+	if (name != NULL && f != NULL)
+		(*f)(name);
+}
