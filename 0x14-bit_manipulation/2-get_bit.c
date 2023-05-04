a7a5cc66875bb0a185131ae2f0fdff90cae62a73
smuhatia <smuhatia>
2023-05-04 18:16:24 +0000

    betty check

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get-bit - this is a function that returns the value of a bit
+ * get_bit - this is a function that returns the value of a bit
  * at a given index
  * @n: This is the value to check for index
  * @index: this is the index of the bit to be accessed
@@ -17,5 +17,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		return (n & 1);
 	}
-	return (get_bit(n >>1, index - 1));
+	return (get_bit(n >> 1, index - 1));
 }
